5867e0bce70e9d25e9376610dd3d2446fe33a61d
Rita Lopes <ritalino99@gmail.com>
2020-11-23 12:50:18 -0300

    Page ook with one calback - Beatiful <3

--- app.py
+++ app.py
@@ -1,28 +1,47 @@
+import plotly.express as px
+import plotly as pl
+import pandas as pd
+import numpy as np
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.express as px
 
-import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('globalterrorismdb.csv', encoding = 'latin-1', low_memory=True)
-df =df[['eventid', 'iyear','imonth', 'iday', 'country_txt', 'region_txt', 'city', 'latitude', 'longitude', 'nkill']]
+df = pd.read_csv('terrorismoGDT.csv', encoding = 'latin-1', low_memory=True)
+df['iyear']= df['iyear'].astype(int)
+countries = np.append(['Todos'], df['country_txt'].sort_values().unique())
 
-countries = df['country_txt'].unique()
+def scatter_attacks(dataframe):
+    paises = pd.DataFrame(dataframe.country_txt.value_counts())
+    paises.columns = ['n_attacks']
+    most_attacked = paises.loc[paises.n_attacks >100]
+    fig = px.scatter(most_attacked,
+                    y= 'n_attacks',
+                    x=most_attacked.index,
+                    color='n_attacks',
+                    marginal_y = 'histogram',
+                    template='plotly_white',
+                    color_continuous_scale=pl.colors.sequential.Bluered,
+                    labels={
+                        'n_attacks': 'Nº ataques',
+                        'index':'Paises'
+                    }
+                    )
+    return fig
+
 
 app.layout = html.Div([
     html.Div([
-
         html.Div([
             dcc.Dropdown(
                 id='country',
                 options=[{'label': i, 'value': i} for i in countries],
-                value='Brazil'
+                value='Todos'
             ),
             dcc.RadioItems(
                 id='dead',
@@ -31,40 +50,51 @@ app.layout = html.Div([
                 labelStyle={'display': 'inline-block'}
             )
         ],
-        style={'width': '48%', 'display': 'inline-block', 'background': '#000000'}),
-    ]),
-
-    dcc.Graph(id='indicator-graphic'),
-
-    dcc.Slider(
-        id='year--slider',
-        min=df['iyear'].min(),
-        max=df['iyear'].max(),
-        value=df['iyear'].max(),
-        marks={str(year): str(year) for year in df['iyear'].unique()},
-        step=None
-    )
-])
+        style={'width': '10%', 'height': '50vh','min-height': '100%','display': 'inline-block', 'margin':'0'}),
+   
+    html.Div([
+           dcc.Graph(id='indicator-graphic'),
+        ],
+        style={'width': '88%', 'height': '50vh','display': 'inline-block', 'float':'right',  'margin':'0'}),
+        html.Div([
+           dcc.Graph(id='scatter-attacks', figure= scatter_attacks(df))
+        ],
+        style={'width': '100%', 'height': '50vh','display': 'inline-block', 'float':'left',  'margin':'0'}),
+    ])  
+], style={'width': '100%', 'height': '50vh'})
 
 @app.callback(
     Output('indicator-graphic', 'figure'),
     [Input('country', 'value'),
-     Input('dead', 'value'),
-     Input('year--slider', 'value')])
-def update_graph(country, dead, year_value):
-    dff = df[df['iyear'] == year_value]
-
+     Input('dead', 'value')])
+def update_graph(country, dead):
+    dff = df
+    z = 1.5
+    if(country !="Todos"):
+      dff = df.loc[df['country_txt'] == country].copy()
+      z=3
+    
+    if(dead =="Com morte"):
+      dff = dff.loc[dff['nkill'] >0].copy()
+    elif(dead =="Sem morte"):
+      dff = dff.loc[dff['nkill'] == 0].copy()
+    px.set_mapbox_access_token(open(".mapbox_token").read())
     fig = px.scatter_mapbox(dff, 
                         lat='latitude',
                         lon='longitude',
-                        text='city',
                         color='iyear',
-                        color_continuous_scale=px.colors.cyclical.IceFire,
-                        mapbox_style='carto-positron')
+                        zoom= z,
+                        opacity = 0.6,
+                        color_continuous_scale=pl.colors.sequential.Jet,
+                        hover_name='city',
+                        labels={
+                            'city':'Cidade',
+                            'nkill':'Mortos',
+                            'iyear': 'Ano'
+                        },
+                        mapbox_style='carto-positron')    
     fig.update_layout(margin={'r':0,'t':0,'l':0,'b':0})
+
     return fig
-
-
-
 if __name__ == '__main__':
-    app.run_server(debug=True, use_reloader=False)
+    app.run_server(debug=True)
